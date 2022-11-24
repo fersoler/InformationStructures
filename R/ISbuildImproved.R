99d410df4c719fd2ac1769a01c65b88a21c326b4
fersoler <fsoler@us.es>
2022-11-24 21:32:29 +0100

    Added ISbuildThirdSorted that uses Piotr's optimizations and sorts the points like the IG algorithm and the original ISbuild function (from smaller to greater communities)

--- R/ISbuildImproved.R
+++ R/ISbuildImproved.R
@@ -321,6 +321,194 @@ ISbuildThird <- function(alphas, gammas){
   )
 }
 
+#### Like ISbuildThird but the points are sorted like in the original version
+ISbuildThirdSorted <- function(alphas, gammas){
+  # STEP 1: Finding the GASSes for all subsystems
+  
+  # Size of the system (number of species)
+  size <- length(alphas)
+  
+  # list to store all stationary points
+  allStatPoints <- list()
+  
+  # Number of stationary points found (trival solution)
+  foundSols <- 1
+  
+  # Trivial solution (0 for all species) is added to the list
+  allStatPoints[[foundSols]] <- as.data.frame(matrix(rep(0.0, size),nrow=1))
+  
+  # list indicating which (sub)communities are equilibrium points. This will be
+  # used in step 2 to set the connectivity. When a new stationary point is found 
+  # with species 'subc', the value of listSubsGASS[n] (being n the number
+  # encoding subc) is set to the index of allStatPoints containing that point.
+  listSubsGASS <- rep(0,2**size)  
+  listSubsGASS[1] <- 1
+  
+  # list indicating index of the stationary point which if the gass of each 
+  # subsystem. After the first step of the algorithm is completed, each 
+  # subsystem will have a gass. 
+  # Value of listGASSindex[scom] is the number of the solution (in the list
+  # allStatPoints) which is the gass of the (sub)community scom (en number 
+  # encoding it).
+  listGASSindex <- rep(0,2**size)
+  listGASSindex[1] <- 1
+  
+  # Data frame to store the GASS in a readable way
+  subsetGASS <- data.frame(subset=character(), ind=integer(), stringsAsFactors=FALSE)
+  
+  # loop to get the GASSes for all subsystems
+  for(s in size:1){
+    allSubS  <-combn(1:size,s)    # All subsystems of size s (from size to 1)
+    for(i in ncol(allSubS):1){  
+      ss     <- allSubS[,i]           # 'ss': current subsystem to look for the GASS
+      subsCode <- getSubPosition(ss)  # Code of the community (listGASSindex)
+      
+      # In case that the GASS of this subcommunity was not found before
+      if(listGASSindex[subsCode] == 0){
+        # Obtaining the GASS
+        gass   <- tryCatch(getGASS_QP(alphas[ss], gammas[ss,ss]), error = function(cond){getGASS_LCP_Lemke(alphas[ss], gammas[ss,ss])})
+        v <- rep(0.0, size)
+        v[ss] <- gass                   # v contains the GASS with 0s
+        gassCode <- getSolPosition(v)   # Code of the obtained gass
+        
+        # Check if the stable point was found before
+        lookAt <- listSubsGASS[gassCode]  # 0 if the stationary point is new
+        if(lookAt == 0){             # New point found
+          foundSols <- foundSols+1   # Increase the number of points found
+          lookAt <- foundSols
+          # Add the new stationary point at the new position:
+          allStatPoints[[lookAt]] <- as.data.frame(matrix(v,nrow=1))
+          # Indicate that the community represented by 'gassCode' is stationary
+          listSubsGASS[gassCode] <- lookAt
+          
+          listGASSindex[subsCode] <- lookAt
+        } 
+        # Indicate the GASS for the current community
+        listGASSindex[subsCode] <- lookAt
+        subsetGASS[nrow(subsetGASS)+1,]<-list(subset=toString(ss), ind=lookAt)
+        
+        # Now look for subcommunities that may have the same GASS
+        # recall that 'ss' is the current community for which subcommunities
+        # with the same GASS are searched
+        eqZeroGASS <- ss[gass == 0]     # positions of 'ss' with 0 
+        if(length(eqZeroGASS) > 0){     # in case there is some 0
+          grZeroGASS <- ss[gass > 0]    # positions with a value >0
+          
+          # Set the GASS for the subcommunity with all > 0
+          if(listGASSindex[getSubPosition(grZeroGASS)] == 0){
+            if(checkGASS_LCP(alphas[grZeroGASS], gammas[grZeroGASS,grZeroGASS], v[grZeroGASS])){
+              listGASSindex[getSubPosition(grZeroGASS)] <- lookAt
+              #subsetGASS[nrow(subsetGASS)+1,]<-list(subset=toString(grZeroGASS), ind=lookAt)
+            }
+          }
+          # Set the GASS for intermediate communities in which was not
+          # previously set and chechGASS_LCP succeeds
+          l2 <- length(eqZeroGASS)
+          # Look at all possible subcommunities (subC) between 'grZeroGASS' and
+          # 'grZeroGASS + eqZeroGASS'
+          if(l2>1){
+            for(n in 1:(2**l2-2)){   
+              subC <- sort(unique(c(grZeroGASS,
+                                    as.integer(intToBits(n)[1:l2])*eqZeroGASS)))[-1]
+              subCNum <- getSubPosition(subC)  # position of the subcomm to look at
+              # In case it was not previously set
+              if(listGASSindex[subCNum] == 0){
+                # and the found gass is also the stationary point of subC
+                if(checkGASS_LCP(alphas[subC], gammas[subC,subC], v[subC])){
+                  # register the gass for that subcommunity
+                  listGASSindex[subCNum] <- lookAt
+                  #subsetGASS[nrow(subsetGASS)+1,]<-list(subset=toString(subC), ind=lookAt)
+                }
+              }
+            }
+          }
+        }
+      } else {
+        subsetGASS[nrow(subsetGASS)+1,]<-list(subset=toString(ss), ind=listGASSindex[getSubPosition(ss)])
+      }
+    }
+  }
+  subsetGASS[nrow(subsetGASS)+1,]<-list(subset="0", ind=1)
+  # number of stationary points
+  nPoints <- foundSols   
+  
+  # convert 'allStatPoints' into a matrix
+  allStatPoints <- as.matrix(do.call(rbind,allStatPoints))
+  
+  # Sorting the points and lists
+  subsetGASS <- subsetGASS[c(2**size:1),]
+  sortingKeys <- unique(subsetGASS[,2])
+  subsetGASS[,2] <- match(subsetGASS[,2],sortingKeys)
+  rownames(subsetGASS) <- 1:2**size
+  # 
+  allStatPoints <- allStatPoints[match(sortingKeys,1:nPoints),]
+  listGASSindex <- match(listGASSindex,sortingKeys)
+  listSubsGASS <- match(listSubsGASS,sortingKeys,nomatch = 0)
+  
+  ##
+  
+  rownames(allStatPoints) <- 1:nPoints  # enumerate rows
+  colnames(allStatPoints) <- c()        # columns
+  
+  
+  
+  # Sorting END
+  
+  
+  
+  
+  
+  # STEP 2: Connecting stationary points
+  # Connectivity matrix
+  connectivity <- matrix(0, nrow = nPoints, ncol = nPoints)
+  # loop to connect all points
+  for(p in 1:nPoints){  # p is the index of the point to look for its connections
+    point <- allStatPoints[p,]    # stationary point
+    I  <- (1:size)[point > 0]     # positions with a value  > 0
+    NI <- (1:size)[point == 0]    # positions with a value == 0
+    if(length(NI) != 0){          # if there are 0 values
+      if(length(I) == 0){   # when all values are 0, we only look at alphas
+        R <- alphas[NI]
+      } else {              # otherwise, compute r_i values
+        R  <- alphas[NI] + gammas[NI,I] %*% as.matrix(point[I])
+      }
+      J <- NI[(1:length(R))[R>0]] # J is the i's with r_i > 0
+      if(length(J) > 0){          # if there are values in J look for connections
+        connectivity[p,unique(listGASSindex[getAllSuperPos(I,J)])] <- 1
+      }
+    }
+  }
+  # Index of the GASS
+  gassInd <- listGASSindex[getSubPosition(1:size)]
+  
+  # Change the order of points to smaller to greater communities
+  # if(nPoints>2){
+  #   if(gassInd>1){
+  #     gassInd <- gassInd-2+nPoints
+  #   }
+  #   #orderP <- c(1,nPoints:2)
+  #   orderP <- listSubsGASS[listSubsGASS>0]
+  #   allStatPoints <- allStatPoints[orderP,]
+  #   rownames(allStatPoints) <- 1:nPoints
+  #   connectivity <- connectivity[orderP,orderP]
+  # }
+  list(
+    #listSubsGASS = listSubsGASS,
+    #listGASSindex = listGASSindex,
+    # The matrix with all stationary points: one point per row
+    points = allStatPoints,
+    # The dataframe indicating the index of the GASS (2n column)
+    # of each subsystem (1st column)
+    subsetGASS   = subsetGASS,
+    # The connectivity matrix. Rows and columns are the indexes of
+    # the points in $points
+    connectivity = connectivity,
+    # Index of the point which is the GASS of the whole system
+    gassInd      = gassInd
+  )
+}
+
+
 
 ## Adding counters to check performance
 ISbuildThirdWithCounters <- function(alphas, gammas){
